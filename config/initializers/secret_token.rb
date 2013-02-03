dbc5920857399f14640e02a7e9f5f0ad1b5ceb7b
Ben Gibbs <ben@bobop.co.uk>
2013-02-03 15:57:18 +0000

    initial commit - yayy

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Borrow::Application.config.secret_token = 'a6ac48bbad58d1077f1a0fa7ec34c3dcb09f8d37d5e05c2d5b61aa9f7c18ffff48c8bc03ec6a999e633bbfe5c8914dbcda2f3cb5569b4d498c8847521262be4e'
