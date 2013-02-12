69aa92000a520666cd0d9658c1f0aef119271cb1
Ben Gibbs <ben@bobop.co.uk>
2013-02-12 20:02:54 +0000

    added Mandrake settings and immediate email for testing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,13 @@ Borrow::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  config.action_mailer.smtp_settings = {
+    :address   => "smtp.mandrillapp.com",
+    :port      => 25, # or 587
+    :enable_starttls_auto => true, # detects and uses STARTTLS
+    :user_name => ENV['MANDRILL_USERNAME'],
+    :password  => ENV['MANDRILL_PASSWORD'],
+    :authentication => 'login' # Mandrill supports 'plain' or 'login'
+  }
 end
