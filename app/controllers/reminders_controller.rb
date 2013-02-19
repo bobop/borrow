fe33c026fe36376f62cf2dbd7a9cb1f50bf3b6ce
Ben Gibbs <ben@bobop.co.uk>
2013-02-19 21:01:23 +0000

    centered notice

--- app/controllers/reminders_controller.rb
+++ app/controllers/reminders_controller.rb
@@ -7,7 +7,15 @@ class RemindersController < ApplicationController
     @reminder = Reminder.new(params[:reminder])
     if @reminder.save
       ReminderMailer.remind_self(@reminder).deliver if Rails.env.development?
-      redirect_to root_url, notice: 'Reminder stored successfully to send later'
+      text = '1 week' if @reminder.reminder_gap == '1w'
+      text = '1 month' if @reminder.reminder_gap == '1m'
+      text = '2 months' if @reminder.reminder_gap == '2m'
+      text = '3 months' if @reminder.reminder_gap == '3m'
+      text = '6 months' if @reminder.reminder_gap == '6m'
+      text = '1 year' if @reminder.reminder_gap == '1y'
+      notice = "Great! Your reminder will be sent in #{text}."
+      notice = "Great! Your reminders will be sent in #{text}." if @reminder.to_email.present?
+      redirect_to root_url, notice: notice
     else
       render :new
     end
